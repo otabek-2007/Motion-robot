d823c23a3341c06934f691694eea2b071b9b3171
otabek-2007 <otabekshavkatov956@gmail.com>
2022-07-17 14:55:06 +0500

    [added] slider carusel

--- JS/app.js
+++ JS/app.js
@@ -28,3 +28,147 @@ window.addEventListener('click', function (event) {
 //         iterator.style.transform=`translateY:120px`
 //     })
 // }
+
+//= = = = = = = = = = = img =  = = = = = = = = = /
+let imagesContainer = [
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-02.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-03.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-04.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+    `<div class="img-item">
+        <div class="img-img">
+            <img src="/IMG/gallery-img-01.jpg" alt="" />
+        </div>
+        <div class="img-text">
+            <h2>Julis dances in the deep darck</h2>
+        </div>
+    </div>`,
+]
+ 
+ let sliderItemsCount=4
+ /* = = = = = = Slider = = = = = = = */
+ 
+const sliderFunc = (array = [], step = 1, count = 4, index = 0) => {
+    const start = index * count;
+    const end = count * (index + 1) ;  
+    const items = array.slice(start, end);
+    let images = ``;
+    for (const item of items) {
+        images += item
+    }
+    return images;
+}
+
+const sliderBtnsCount = (len, count) => { 
+    if (len <= 0 || count <= 0) return;
+    const val = len / count; 
+    if (Math.floor(val) == val) return val;   
+    else return Math.ceil(val)
+}
+
+/* 
+14 ta element bor, 4 tadan chiqar
+
+        span-1 => 0 - 4
+            index * count - (index * count + (count - 1))
+
+            0 - 3
+        span-2 => 1 - 4
+            index * count - (index * count + (count - 1))
+            4 - 7 
+        span-3 => 2 - 4
+        span-4 => 3 - 2
+*/
+
+/* = = = = = = Slider = = = = = = = */
+
+//  buttons in slider
+const sliderBtnsContainer = document.querySelector('.balon-container')
+
+let sliderButtons = ``;
+for (let index = 0; index < sliderBtnsCount(imagesContainer.length,sliderItemsCount); index++) {
+    sliderButtons += `<span></span>`
+}
+
+sliderBtnsContainer.innerHTML = sliderButtons;
+
+const sliderBtns = document.querySelectorAll('.balon-container span')
+
+const imagesContent = document.querySelector('.img-content')
+
+imagesContent.innerHTML = sliderFunc(imagesContainer, 1, sliderItemsCount, 0)
+
+for (const [index,btn] of sliderBtns.entries()) {
+    btn.addEventListener('click', function () {
+        imagesContent.innerHTML = sliderFunc(imagesContainer, 1, sliderItemsCount, index)
+    })
+}
+
